f5e1f2908b91f5e20b89c0f93688ff89cd02257b
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-08-13 17:32:23 +0900

    refactor: OAuth & Authorization 로직 완성

--- backend/node/app/src/util/redis.service.ts
+++ backend/node/app/src/util/redis.service.ts
@@ -1,14 +1,16 @@
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { Cache } from 'cache-manager';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class RedisService {
-  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+  constructor(
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+    private readonly configService: ConfigService,
+  ) {}
 
   async set(key: string, value: string) {
-    console.log(key);
-    console.log(value);
-    await this.cacheManager.set(key, value);
+    await this.cacheManager.set(key, value, { ttl: 0 });
   }
 
   async get(key: string): Promise<string> {
