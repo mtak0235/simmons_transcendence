e0ac701d1181ae29657a49b2678ec41cdac4f8ab
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-08-14 15:22:34 +0900

    delete: events logic

--- backend/node/app/src/events/events.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-
-@Injectable()
-export class EventsMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
-    console.log(req);
-    next();
-  }
-}
