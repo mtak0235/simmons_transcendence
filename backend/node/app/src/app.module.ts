9c4b47cfab4c49b0be2b5c872fc2c71af14e3067
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-08-14 15:23:24 +0900

    refactor: delete app MVC source

--- backend/node/app/src/app.module.ts
+++ backend/node/app/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from '@src/app.controller';
-import { AppService } from '@src/app.service';
 import { AuthModule } from '@auth/auth.module';
 import { UserModule } from '@user/user.module';
 import { UtilModule } from '@util/util.module';
 
 @Module({
   imports: [UtilModule, AuthModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
