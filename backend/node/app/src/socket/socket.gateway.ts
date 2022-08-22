c131d17b76f4db30d4c563e1ee1f3a220e1e61f0
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2022-08-22 13:03:52 +0900

    refactor: gateway 도메인 별 분리

--- backend/node/app/src/socket/socket.gateway.ts
+++ backend/node/app/src/socket/socket.gateway.ts
@@ -30,12 +30,15 @@ export class Client extends Socket {
 export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
+
+  // todo: delete: 어디에 사용해야할 지 아직 모르겠음
   private logger: Logger = new Logger('SocketGateway');
 
+  // todo: delete: 어디에 사용해야할 지 아직 모르겠음
   @Inject('socketInterceptor')
   private eventInterceptor: SocketInterceptor;
+
   constructor(
-    // private readonly socketService: SocketService,
     private readonly mainSocketService: MainSocketService,
     private readonly userSocketService: UserSocketService,
     private readonly channelSocketService: ChannelSocketService,
@@ -43,7 +46,7 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
   ) {}
 
   /* ============================================= */
-  /*              Handling Connection              */
+  /*            #1 Handling Connection             */
   /* ============================================= */
 
   async handleConnection(client: Client) {
@@ -79,6 +82,7 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
     }
   }
 
+  // todo: delete: 개발용 코드
   @SubscribeMessage('testUpdate')
   testUpdate(
     @ConnectedSocket() client: Client,
@@ -90,171 +94,86 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
     console.log(client.user);
   }
 
-  // @SubscribeMessage('inChannel')
-  // inChannel(
-  //   @MessageBody('channelId')
-  //   channelId: string,
-  //   @ConnectedSocket()
-  //   client,
-  // ) {
-  //   this.socketService.enterChannel(client, channelId);
-  // }
-  //
-  // @SubscribeMessage('outChannel')
-  // outChannel(
-  //   @ConnectedSocket()
-  //   client: Client,
-  // ) {
-  //   this.socketService
-  //     .getChannelFullName(client.rooms, /^room:user:/)
-  //     .forEach((channelName) => {
-  //       client
-  //         .to(channelName)
-  //         .emit('getMessage', `${client.user.username}님이 퇴장하셨습니다.`);
-  //       client.leave(channelName);
-  //     });
-  //   client.broadcast.emit('outChannel', client.user.userId);
-  // }
-  //
-  // @SubscribeMessage('block')
-  // block(
-  //   @MessageBody('badGuyID') badGuyID: number,
-  //   @ConnectedSocket() client: Client,
-  // ) {
-  //   this.socketService.block(badGuyID, client.user.userId);
-  // }
-  //
-  // @SubscribeMessage('follow')
-  // follow(
-  //   @MessageBody('targetID') targetID: number,
-  //   @ConnectedSocket() client: Client,
-  // ) {
-  //   this.socketService.friendChanged({
-  //     userId: client.user.userId,
-  //     targetID,
-  //     isFollowing: true,
-  //   });
-  //   this.server
-  //     .to(targetID.toString())
-  //     .to(client.user.userId.toString())
-  //     .emit('friendChanged', {
-  //       userId: client.user.userId,
-  //       targetID,
-  //       isFollowing: true,
-  //     });
-  // }
-  //
-  // @SubscribeMessage('unfollow')
-  // unfollow(
-  //   @MessageBody('targetID') targetID: number,
-  //   @ConnectedSocket() client: Client,
-  // ) {
-  //   this.socketService.friendChanged({
-  //     userId: client.user.userId,
-  //     targetID,
-  //     isFollowing: false,
-  //   });
-  //   this.server.to(targetID.toString()).emit('friendChanged', {
-  //     userId: client.user.userId,
-  //     targetID,
-  //     isFollowing: false,
-  //   });
-  // }
-  //
-  // @SubscribeMessage('sendMSG')
-  // sendMSG(@MessageBody('msg') msg: string, @ConnectedSocket() client: Client) {
-  //   client
-  //     .to(this.socketService.getChannelFullName(client.rooms, /^room:user:/))
-  //     .emit('getMSG', {
-  //       userId: client.user.userId,
-  //       username: client.user.username,
-  //       msg,
-  //     });
-  // }
-  //
-  // @SubscribeMessage('sendDM')
-  // sendDM(
-  //   @MessageBody('recipientId') recipientTd: string,
-  //   @MessageBody('msg') msg: string,
-  //   @ConnectedSocket() client: Client,
-  // ) {
-  //   this.socketService.saveMessage(msg);
-  //   client
-  //     .to(this.socketService.getChannelFullName(client.rooms, /^room:user:/))
-  //     .emit('getDM', {
-  //       userId: client.user.userId,
-  //       username: client.user.username,
-  //       msg,
-  //     });
-  // }
-  //
-  // @SubscribeMessage('kickOut')
-  // kickOut(client: Client, badGuyID: number) {
-  //   this.socketService.kickOut(client, badGuyID, this.server);
-  //   this.outChannel(client);
-  // }
-  //
-  // @SubscribeMessage('modifyGame')
-  // modifyGame(
-  //   @ConnectedSocket() client: Client,
-  //   @MessageBody() channelInfo: ChannelInfoDto,
-  // ) {
-  //   this.socketService.modifyGame(client, channelInfo);
-  // }
-  //
-  // @SubscribeMessage('inviteUser')
-  // inviteUser(
-  //   @MessageBody('invitedUserId') invitedUserId: number,
-  //   @ConnectedSocket() client: Client,
-  // ) {
-  //   this.socketService.inviteUser(client, invitedUserId, this.server);
-  // }
-  //
-  // @SubscribeMessage('mute')
-  // mute(
-  //   @MessageBody('noisyGuyId') noisyGuyId: number,
-  //   @ConnectedSocket() client: Client,
-  // ) {
-  //   this.socketService.mute(client, noisyGuyId);
-  // }
-  //
-  // @SubscribeMessage('generateGame')
-  // generateGame(
-  //   @ConnectedSocket() client: Client,
-  //   @MessageBody() channelInfoDto: ChannelInfoDto,
-  // ) {
-  //   this.socketService.createChannel(client, channelInfoDto);
-  // }
-  //
-  // @SubscribeMessage('endGame')
-  // endGame(
-  //   @ConnectedSocket() client: Client,
-  //   @MessageBody('score')
-  //   gameResult: { winner: number; loser: number; score: number },
-  // ) {
-  //   this.socketService.saveGameResult(gameResult);
-  //   return { event: 'endGame', data: gameResult };
-  // }
-  //
-  // @SubscribeMessage('waitingGame')
-  // waitingGame(@ConnectedSocket() client: Client) {
-  //   this.socketService.reserveGame(client);
-  // }
-  //
-  // @SubscribeMessage('readyGame')
-  // readyGame(client: Client) {
-  //   this.socketService.readyGame(client, this.server);
-  // }
+  /* ============================================= */
+  /*              #2 Channel Gateway               */
+  /* ============================================= */
 
-  // @SubscribeMessage('client2Server')
-  // handleMessage(client: any) {
-  //   // this.server.emit('server2Client', data);
-  //   return { socket: 'client2Server', data: 'Hello world!' };
-  // }
-  // @SubscribeMessage('events')
-  // findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-  //   return from([1, 2, 3]).pipe(
-  //     map((item) => ({ socket: 'events', data: item })),
-  //   );
-  // }
+  @SubscribeMessage('createChannel')
+  createChannel(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('modifyChannel')
+  modifyGame(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('inChannel')
+  inChannel(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('outChannel')
+  outChannel(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('inviteUser')
+  inviteUser(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('kickOutUser')
+  kickOutUser(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('muteUser')
+  muteUser(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('waitingGame')
+  waitingGame(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('readyGame')
+  readyGame(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('endGame')
+  endGame(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('sendMSG')
+  sendMSG(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('sendDM')
+  sendDM(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  /* ============================================= */
+  /*                #2 User Gateway                */
+  /* ============================================= */
+
+  @SubscribeMessage('blockUser')
+  blockUser(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('followUser')
+  followUser(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
+
+  @SubscribeMessage('unfollowUser')
+  unfollowUser(@ConnectedSocket() client: Client) {
+    // todo: development
+  }
 }
